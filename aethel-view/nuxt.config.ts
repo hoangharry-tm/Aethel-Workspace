2c9b7912645f2634a2c7548c162e4194b9c32cb3
hoangharry-tm <tonminhhoang.work@gmail.com>
2026-05-24 21:34:26 +0700

    update blueprints examples
    Signed-off-by: hoangharry-tm <tonminhhoang.work@gmail.com>

--- aethel-view/nuxt.config.ts
+++ aethel-view/nuxt.config.ts
@@ -1,18 +1,21 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxtjs/mcp-toolkit',
-    '@oro.ad/nuxt-claude-devtools',
-    '@pinia/nuxt'
-  ]
-})
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@nuxtjs/mcp-toolkit",
+    "@oro.ad/nuxt-claude-devtools",
+    "@pinia/nuxt",
+  ],
+});
+
